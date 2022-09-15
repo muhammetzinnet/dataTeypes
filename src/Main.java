3699cb62b0432ef48ea8f1d9046aa5bb865bdb63
https://github.com/muhammetzinnet/Veriable <maicek@hotmail.com>
2022-09-15 22:47:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte sayi_1 = 12;
+        double sayi_2 = 15.6;
+        char karakter = 'a';
+        boolean dogruMu = false;
+        System.out.println(sayi_1);
+        System.out.println(sayi_2);
+        System.out.println(karakter);
+        System.out.println(dogruMu);
+    }
+}
